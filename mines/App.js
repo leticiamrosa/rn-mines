421b33fc78b0f53be784ef36e2ee9dd7ac0f127c
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2018-11-05 23:12:16 -0200

    add mines and field

--- mines/App.js
+++ mines/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import params from './src/params';
 
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Iniciando Campo Minado!!</Text>
+        <Text style={styles.instructions}>
+        Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field/>
+        <Field opened/>
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
+        <Field mined/>
+        <Field mined opened/>
+        <Field mined opened exploded/>
       </View>
     );
   }
